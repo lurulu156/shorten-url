bd83750e655b616569cb1e437f5649bd3e60fbff
lurulu156 <kzzlin9@gmail.com>
2022-11-18 11:54:24 +0800

    feat: adding mongoose and connect to mongoDB

--- app.js
+++ app.js
@@ -3,12 +3,28 @@ const app = express()
 const PORT = 3000
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
 //set up handlebars
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+// 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+// MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })//避開警告訊息
+// MongoDB-connection status
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 //main page
 app.get('/', (req, res) => {
