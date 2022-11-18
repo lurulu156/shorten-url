9427934b3e4a0a800d2435f0196a4738ac9107c4
lurulu156 <kzzlin9@gmail.com>
2022-11-18 11:46:17 +0800

    feat: adding bodyparser

--- app.js
+++ app.js
@@ -2,16 +2,23 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//set up handlebars
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+//main page
 app.get('/', (req, res) => {
   res.render('index')
 })
 
+//create short url
 app.post('/', (req, res) => {
-  res.render('url')
+  const originalURL = req.body.original_URL
+  res.render('url', { originalURL })
 })
 
 app.listen(PORT, () => {
