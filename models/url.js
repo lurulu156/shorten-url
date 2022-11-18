883f79d7ce4aa988779c1daf28d3e5dd3a449df2
lurulu156 <kzzlin9@gmail.com>
2022-11-18 12:01:26 +0800

    feat: adding url schema

---
+++ models/url.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const urlSchema = new Schema({
+  originalURL: {
+    type: String, 
+    required: true 
+  },
+  newURL: {
+    type: String, 
+    required: true 
+  }
+})
+module.exports = mongoose.model('URL', urlSchema)
